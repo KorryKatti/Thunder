3b1dfaf5deb99ac2d2fe56655261313ccc9917d7
KorryKatti <143781663+KorryKatti@users.noreply.github.com>
2024-03-12 10:21:53 +0530

    Update index.py

--- index.py
+++ index.py
@@ -7,12 +7,5 @@ customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), da
 app = customtkinter.CTk()  # create CTk window like you do with the Tk window
 app.geometry("400x240")
 app.resizable(True, True)
-
-def button_function():
-    print("button pressed")
-
-# Use CTkButton instead of tkinter Button
-button = customtkinter.CTkButton(master=app, text="CTkButton", command=button_function)
-button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
-
+app.title("Thunder 🗲")
 app.mainloop()
