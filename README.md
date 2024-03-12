88c9c73c4c5146804380fbed1a5c94454b66891b
KorryKatti <143781663+KorryKatti@users.noreply.github.com>
2024-03-12 10:13:40 +0530

    seems cool so far , tkinter in arch linux is a real trouble maker tho

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Thunder
 i think i am up to something
+
+[ first time using virtual env , idk how it is done]
---
+++ index.py
@@ -0,0 +1,18 @@
+
+import customtkinter
+
+customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
+customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
+
+app = customtkinter.CTk()  # create CTk window like you do with the Tk window
+app.geometry("400x240")
+app.resizable(True, True)
+
+def button_function():
+    print("button pressed")
+
+# Use CTkButton instead of tkinter Button
+button = customtkinter.CTkButton(master=app, text="CTkButton", command=button_function)
+button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
+
+app.mainloop()
