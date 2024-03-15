1be183518adbca41381d775d7687c8fbc3a8cf9f
KorryKatti <143781663+KorryKatti@users.noreply.github.com>
2024-03-15 10:35:57 +0530

    Update run_main.sh

--- run_main.sh
+++ run_main.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
+# Activate the virtual environment
 source myenv/bin/activate
-python -c "import sys; sys.exit(0 if __import__('importlib.util').find_spec('customtkinter') else 1)"
+
+# Attempt to import the module directly in Python
+python -c "import sys, importlib; sys.exit(0 if importlib.util.find_spec('customtkinter') else 1)"
+
+# Check the exit status of the previous command
 if [ $? -eq 0 ]; then
     python main.py
 else
