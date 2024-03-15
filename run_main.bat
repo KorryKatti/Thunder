cf7f4e60fcf91124482c9c0dc0dbbdac911d5b26
KorryKatti <143781663+KorryKatti@users.noreply.github.com>
2024-03-15 08:50:44 +0530

    Update run_main.bat

--- run_main.bat
+++ run_main.bat
@@ -1,5 +1,4 @@
 @echo off
-call myenv\Scripts\activate.bat
 python -c "import sys; sys.exit(0 if __import__('importlib.util').find_spec('customtkinter') else 1)"
 if %errorlevel% equ 0 (
     python main.py
