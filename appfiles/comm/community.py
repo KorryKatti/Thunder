41b037a0d4ec4220eb26aef5139556a6dbf075a9
KorryKatti <143781663+KorryKatti@users.noreply.github.com>
2024-04-01 11:40:39 +0530

    bruh

--- appfiles/comm/community.py
+++ appfiles/comm/community.py
@@ -1,23 +1,26 @@
 import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout
 from PyQt5.QtCore import QUrl
-from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 
-class MainWindow(QMainWindow):
-    def __init__(self):
+class WebWindow(QMainWindow):
+    def __init__(self, url):
         super().__init__()
+        
         self.setWindowTitle("Embedded Web Page")
         self.setGeometry(100, 100, 800, 600)
-        
+
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+        self.layout = QVBoxLayout(self.central_widget)
+
         self.browser = QWebEngineView()
-        self.setCentralWidget(self.browser)
-        
-        # Load URL
-        url = QUrl("https://www.wikipedia.org")
-        self.browser.setUrl(url)
+        self.layout.addWidget(self.browser)
+        self.browser.load(QUrl(url))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = MainWindow()
+    url = "https://www.example.com"
+    window = WebWindow(url)
     window.show()
     sys.exit(app.exec_())
