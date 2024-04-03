1d32e649b9922e7ac02ca025622c8c98a910f7a4
KorryKatti <143781663+KorryKatti@users.noreply.github.com>
2024-04-03 16:44:11 +0530

    I am failing my most important exams

--- appfiles/comm/community.py
+++ appfiles/comm/community.py
@@ -1,26 +1,21 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtCore import QUrl
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 
-class WebWindow(QMainWindow):
-    def __init__(self, url):
+class MainWindow(QMainWindow):
+    def __init__(self):
         super().__init__()
-        
-        self.setWindowTitle("Embedded Web Page")
+
+        self.setWindowTitle("Embedded Web Browser")
         self.setGeometry(100, 100, 800, 600)
 
-        self.central_widget = QWidget()
-        self.setCentralWidget(self.central_widget)
-        self.layout = QVBoxLayout(self.central_widget)
+        web_view = QWebEngineView()
+        web_view.setUrl(QUrl('https://example.com'))  # Create QUrl object from URL string
+        self.setCentralWidget(web_view)
 
-        self.browser = QWebEngineView()
-        self.layout.addWidget(self.browser)
-        self.browser.load(QUrl(url))
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app = QApplication(sys.argv)
-    url = "https://www.example.com"
-    window = WebWindow(url)
+    window = MainWindow()
     window.show()
     sys.exit(app.exec_())
