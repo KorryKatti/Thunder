338b883a2c21fc10d376fe72576c33eb406f07ee
KorryKatti <143781663+KorryKatti@users.noreply.github.com>
2024-04-04 16:07:23 +0530

    kmsfr

--- open.py
+++ open.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import subprocess
 import sys
 
@@ -14,20 +15,24 @@ def create_venv(directory):
 def install_customtkinter(venv_directory):
     os.system(f"{os.path.join(venv_directory, 'bin', 'python')} -m pip install customtkinter")
 
-# Function to run main.py using the Python interpreter from the virtual environment
-def run_main_py(venv_directory):
-    python_executable = os.path.join(venv_directory, "bin", "python")
-    main_script = "main.py"
-    subprocess.run([python_executable, main_script])
+# Function to activate the virtual environment
+def activate_venv(directory):
+    activate_script = os.path.join(directory, "bin", "activate")
+    return f"source {activate_script}"
 
-# Function to run updater.py using the Python interpreter from the virtual environment
-def run_updater_py(venv_directory):
+# Function to run main.py or updater.py using the Python interpreter from the virtual environment
+def run_script_py(venv_directory, script_name):
     python_executable = os.path.join(venv_directory, "bin", "python")
-    updater_script = "updater.py"
-    subprocess.run([python_executable, updater_script])
+    script_path = f"{script_name}.py"
+    subprocess.run([python_executable, script_path])
 
 def main():
     venv_directory = "myenv"
+
+    # Delete the "update" folder if it exists
+    update_folder = "update"
+    if directory_exists(update_folder):
+        shutil.rmtree(update_folder)
 
     if not directory_exists(venv_directory):
         create_venv(venv_directory)
@@ -37,11 +42,12 @@ def main():
         import customtkinter
     except ImportError:
         # If not importable, run updater.py
-        run_updater_py(venv_directory)
+        activate_command = activate_venv(venv_directory)
+        subprocess.run(f"{activate_command} && python updater.py", shell=True)
         return
 
     install_customtkinter(venv_directory)
-    run_main_py(venv_directory)
+    run_script_py(venv_directory, "main")
 
 if __name__ == "__main__":
     main()
