f3a1400f8b5cc3d79af81eecc5b4d42368723420
KorryKatti <143781663+KorryKatti@users.noreply.github.com>
2024-04-08 13:22:22 +0530

    Update open.py

--- open.py
+++ open.py
@@ -42,7 +42,8 @@ def install_customtkinter(venv_directory):
 
 # Function to run main.py or updater.py using the Python interpreter from the virtual environment
 def run_script_py(venv_directory, script_name):
-    python_executable = os.path.join(venv_directory, "Scripts" if os.name == 'nt' else "bin", "python" + (".exe" if os.name == 'nt' else ""))
+    python_executable = os.path.join(venv_directory, "Scripts", "python.exe" if os.name == 'nt' else "bin", "python")
+
     script_path = f"{script_name}.py"
     subprocess.run([python_executable, script_path])
 
